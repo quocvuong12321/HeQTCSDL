5b7d29b5000d695e5c6dd8fd5b9dc0c28f705e6e
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-10-29 21:57:27 +0700

    Them layout

---
+++ QuanLy_TourDuLich/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLy_TourDuLich.Models;
+namespace QuanLy_TourDuLich.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
